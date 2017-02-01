b781aaf63d5d4bc2838c184306759bc894d558d3
Abeneazer Chafamo <achafamo@colgate.edu>
2017-01-31 21:31:57 -0500

    First commit

---
+++ Compgeo_project/__init__.py
@@ -0,0 +1,3 @@
+"""
+Package for Compgeo_project.
+"""
---
+++ env_compgeo/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\achaf\onedrive\documents\visual studio 2015\Projects\Compgeo-project\Compgeo-project\env_compgeo\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
